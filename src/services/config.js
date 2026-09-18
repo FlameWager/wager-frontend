9a9cc6fa3c25774ca7ea177858389448c91594da
huydo2105 <herohthd@gmail.com>
2026-09-18 06:46:26 +0200

    chore: add vercel rewrite configuration and lowercase testnet address

--- src/services/config.js
+++ src/services/config.js
@@ -118,7 +118,7 @@ export const verifiedMakers = {
   testnet: [
     "0x327292fe3a8c8b9c4ae65fd67f742b10c4e61a55",
     "0x96e73a6b2682a0b34af98a288ad46b8bc9b779b2",
-    "0x82837e2FA6aA8D5B686d1bCc9A24a3a1DcCB22b6",
+    "0x82837e2fa6aa8d5b686d1bcc9a24a3a1dccb22b6",
   ],
   mainnet: [
     "0x327292fE3A8C8B9C4aE65FD67f742B10C4E61A55",
